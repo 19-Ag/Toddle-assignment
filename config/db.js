fca26addf4b8fdc028c35a1f817169d0e9a7e04f
Pranshu <pranshuupadhayay@gmail.com>
2025-05-18 15:26:55 +0530

    Initial commit

--- config/db.js
+++ config/db.js
@@ -1,11 +1,3 @@
 const { Sequelize } = require('sequelize');
-const sequelize = new Sequelize(process.env.DB_URL || 'postgres://postgres:postgres@localhost:5432/journal_toddle' , {
-  dialect: 'postgres',
-  dialectOptions: {
-    ssl: {
-      require: true, // Enforce SSL
-      rejectUnauthorized: false, // Allow self-signed certificates
-    },
-  },
-});
+const sequelize = new Sequelize(process.env.DB_URL || 'postgres://postgres:postgres@localhost:5432/journal_toddle');
 module.exports = sequelize;
